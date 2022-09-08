8f9c368eb79881b251b168bc56440f4c3cf888a0
MingxuanAWA <84988050+MingxuanAWA@users.noreply.github.com>
2022-09-09 07:33:34 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,61 +2,24 @@
 
 set -e
 
-# check values
-
-if [ -n "${PUBLISH_REPOSITORY}" ]; then
-    TARGET_REPOSITORY=${PUBLISH_REPOSITORY}
-else
-    TARGET_REPOSITORY=${GITHUB_REPOSITORY}
-fi
-
-if [ -n "${BRANCH}" ]; then
-    TARGET_BRANCH=${BRANCH}
-else
-    TARGET_BRANCH="gh-pages"
-fi
-
-if [ -n "${PUBLISH_DIR}" ]; then
-    TARGET_PUBLISH_DIR=${PUBLISH_DIR}
-else
-    TARGET_PUBLISH_DIR="./public"
-fi
-
-# deploy to
-echo ">>>>> Start deploy to ${TARGET_REPOSITORY} <<<<<"
-
-# Installs Git.
+echo ">>>>> Start deploy to MingxuanAWA/blog <<<<<"
 echo ">>> Install Git ..."
 apt-get update && \
 apt-get install -y git && \
-
-# Directs the action to the the Github workspace.
-cd "${GITHUB_WORKSPACE}"
-
-echo ">>> Install NPM dependencies ..."
-npm install
-
-echo ">>> Clean folder ..."
-npx hexo clean
-
-echo ">>> Generate file ..."
-npx hexo generate
-
-echo ">>> Setup ssh-private-key"
 mkdir -p /root/.ssh/
 echo "$DEPLOY_KEY" > /root/.ssh/id_rsa
 chmod 600 /root/.ssh/id_rsa
 ssh-keyscan -t rsa github.com >> /root/.ssh/known_hosts
-
-echo ">>> Config git ..."
-
-git clone "git@github.com:${TARGET_REPOSITORY}" .deploy_git
-
-# Configures Git.
-git config --global user.name "MingxuanAWA"
-git config --global user.email "mxgame@foxmail.com"
-
-echo '>>> Start Push ...'
-npx hexo deploy -m "Deploy to ${TARGET_REPOSITORY}"
-
+cd "${GITHUB_WORKSPACE}"
+npm install
+npx hexo clean
+npx hexo generate
+git clone git@github.com:MingxuanAWA/blog repo
+cp -rf ${TARGET_PUBLISH_DIR}/* repo
+cd repo
+git config user.name "MingxuanAWA"
+git config user.email "mxgame@foxmail.com"
+git add .
+git commit --allow-empty -m "Deploy to MingxuanAWA/blog"
+git push origin master --force
 echo ">>> Deployment successful!"
