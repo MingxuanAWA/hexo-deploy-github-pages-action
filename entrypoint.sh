9dd45566225ea33c91cad5351c9d35b0dcd56fc0
MingxuanAWA <84988050+MingxuanAWA@users.noreply.github.com>
2022-09-09 07:53:00 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,13 +14,14 @@ cd "${GITHUB_WORKSPACE}"
 npm install
 npx hexo clean
 npx hexo generate
+git clone git@github.com:MingxuanAWA/blog repo
 cp -rf ${TARGET_PUBLISH_DIR}/* repo
 cd repo
-git clone git@github.com:MingxuanAWA/blog repo
 git config user.name "MingxuanAWA"
 git config user.email "mxgame@foxmail.com"
 git add .
 git commit --allow-empty -m "Deploy to MingxuanAWA/blog"
 git push origin master --force
 echo ">>> Deployment successful!"
+
 
